d9246fe2b3bfbb0e7f49705b4a098927492d5ae7
amiraflak <mehrdad.aflakparast@gmail.com>
2023-09-29 22:33:30 +0330

    feat: to customize instance name

--- instance_control.sh
+++ instance_control.sh
@@ -1,27 +1,30 @@
+bash
 #!/bin/bash
+
+# Set your instance name here
+INSTANCE_NAME="mehrdad-gcp"
 
 start_instance() {
   # Start the instance and capture the external_ip
-  external_ip=$(gcloud compute instances start mehrdad-gcp --format="value(networkInterfaces[0].accessConfigs[0].natIP)")
+  external_ip=$(gcloud compute instances start "$INSTANCE_NAME" --format="value(networkInterfaces[0].accessConfigs[0].natIP)")
 
   # Sleep for 10 seconds to allow time for the instance to start up
-  sleep 1
-  echo " new hiiiiiiiiiiiiii"
+  sleep 10
+
   # Find the entry in .ssh/config corresponding to the instance name
-  entry=$(awk -v instance="mehrdad-gcp" '$2 == instance { print NR + 1 }' ~/.ssh/config)
+  entry=$(awk -v instance="$INSTANCE_NAME" '$2 == instance { print NR + 1 }' ~/.ssh/config)
   # echo "Entry number is: ${entry}"
-  echo "hiiiiiiiiiiiiii"
 
   # Replace the IP in the .ssh/config file
-  # sed -i "${entry}s/Host .*$/Host mehrdad-gcp\n    HostName ${external_ip}/" ~/.ssh/config
+  # sed -i "${entry}s/Host .*$/Host $INSTANCE_NAME\n    HostName ${external_ip}/" ~/.ssh/config
   sed -i "${entry}s/.*/  HostName ${external_ip} /" ~/.ssh/config
 
-  echo "External IP replaceeeeeed with: ${external_ip}"  
+  echo "External IP replaced with: ${external_ip}"
 }
 
 stop_instance() {
   # Stop the instance and capture the output
-  stop_output=$(gcloud compute instances stop mehrdad-gcp)
+  stop_output=$(gcloud compute instances stop "$INSTANCE_NAME")
 
   # Print the stop output
   echo "$stop_output"
@@ -39,11 +42,4 @@ case "$1" in
         echo "Usage: $0 [start|stop]"
         exit 1
         ;;
-esac    
-
-
-
-# Host mehrdad-gcp
-#   HostName 34.90.253.113
-#   User mehrdad
-#   IdentityFile ~/.ssh/gcp
+esac
